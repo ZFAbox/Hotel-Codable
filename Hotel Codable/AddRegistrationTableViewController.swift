6ab990f84fb6d6e5c67585f340aca44632533c25
Fedor <starbreeze@yandex.ru>
2025-07-31 23:53:58 +0300

    Update Date Pickers

--- Hotel Codable/AddRegistrationTableViewController.swift
+++ Hotel Codable/AddRegistrationTableViewController.swift
@@ -9,15 +9,30 @@ import UIKit
 
 class AddRegistrationTableViewController: UITableViewController {
 
+    //Section 1
     @IBOutlet weak var firstNameTextField: UITextField!
-    
     @IBOutlet weak var seconfNameTextField: UITextField!
-    
     @IBOutlet weak var emailTextField: UITextField!
     
+    //Section 2
+    @IBOutlet weak var checkInDateLabel: UILabel!
+    @IBOutlet weak var checkInDatePicker: UIDatePicker!
+    @IBOutlet weak var checkOutDateLabel: UILabel!
+    @IBOutlet weak var checkOutDatePicker: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let midnightTodayDate = Calendar.current.startOfDay(for: Date())
+        checkInDatePicker.minimumDate = midnightTodayDate
+        checkInDatePicker.date = midnightTodayDate
+        
+        checkOutDatePicker.minimumDate = Calendar.current.date(byAdding: .day, value: 1, to: midnightTodayDate)
+        updateDateViews()
+    }
+    
+    private func updateDateViews() {
+        checkInDateLabel.text = checkInDatePicker.date.formatted(date: .abbreviated, time: .omitted)
+        checkOutDateLabel.text = checkOutDatePicker.date.formatted(date: .abbreviated, time: .omitted)
     }
     
     @IBAction func doneBarButton(_ sender: Any) {
@@ -31,5 +46,10 @@ class AddRegistrationTableViewController: UITableViewController {
         print("Second Name: \(secondName)")
         print("Email: \(email)")
     }
+    
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        updateDateViews()
+    }
+    
     
 }
