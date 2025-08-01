8981ed5ca8d353dac06e9acdf399e08b60ef34f1
ZFAbox <starbreeze@yandex.ru>
2025-08-01 07:05:11 -0700

    update picker

--- Hotel Codable/AddRegistrationTableViewController.swift
+++ Hotel Codable/AddRegistrationTableViewController.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 class AddRegistrationTableViewController: UITableViewController {
-
+    
+    private var checkInDatePickerVisibale: Bool = false {
+        didSet {
+            checkInDatePicker.isHidden = !checkInDatePickerVisibale
+        }
+    }
+    private var checkOutDatePickerVisible: Bool = false {
+        didSet {
+            checkOutDatePicker.isHidden = !checkOutDatePickerVisible
+        }
+    }
+    
+    private let checkInDatePickerIndexPath = IndexPath(row: 1, section: 1)
+    private let checkOutDatePickerIndexPath = IndexPath(row: 3, section: 1)
+    
     //Section 1
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var seconfNameTextField: UITextField!
@@ -40,16 +54,46 @@ class AddRegistrationTableViewController: UITableViewController {
         let firstName = firstNameTextField.text ?? ""
         let secondName = seconfNameTextField.text ?? ""
         let email = emailTextField.text ?? ""
+        let checkInDate = checkInDateLabel.text ?? ""
+        let checkOutDate = checkOutDateLabel.text ?? ""
         
         print("Done button tapped!")
         print("First Name: \(firstName)")
         print("Second Name: \(secondName)")
         print("Email: \(email)")
+        print("Check-In Date: \(checkInDate)")
+        print("Check-OUt Date: \(checkOutDate)")
     }
     
     @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
         updateDateViews()
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case checkInDatePickerIndexPath where checkInDatePickerVisibale == false :
+            return 0
+        case checkOutDatePickerIndexPath where checkOutDatePickerVisible == false :
+            return 0
+        default:
+            return UITableView.automaticDimension
+            
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case checkInDatePickerIndexPath:
+            return 190
+        case checkOutDatePickerIndexPath:
+            return 190
+        default:
+            return UITableView.automaticDimension
+        }
+    }
     
 }
